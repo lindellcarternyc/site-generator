0f87ab6c84d683d4c4790f1ff5f391d5d9e80c1c
Lindell Carter <lindellcarternyc@gmail.com>
2017-12-13 15:11:50 -0500

    Updated src/ with utility functions for locating template files (.pug)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,48 @@
-export const Test = (): string => {
-  return 'test'
+import * as FS from 'fs'
+import * as Path from 'path'
+
+interface Folder {
+  name: string
+  filenames: string[]
+}
+
+export const getViewsFolder = (): string[] => {
+  const viewsPath = Path.resolve(process.cwd(), 'views')
+  return FS.readdirSync(viewsPath)
+}
+
+export const getTemplateFolder = (foldername: string = 'views'): Folder => {
+  const folderPath = Path.resolve(process.cwd(), foldername)
+  const filenames = FS.readdirSync(folderPath)
+
+  return {
+    name: foldername,
+    filenames
+  }
+}
+
+interface Template {
+  path: string
+}
+
+interface TemplateCollection {
+  [key: string]: Template
+}
+
+export const getTemplateFilesFromFolder = (folder: Folder): TemplateCollection => {
+  const collection: TemplateCollection = { }
+  const templateFileNames = folder.filenames.filter(filename => {
+    return Path.extname(filename) === '.pug'
+  })
+  
+  templateFileNames.forEach(filename => {
+    const templateName = filename.slice(0, filename.length - 4)
+    const templatePath = Path.resolve(process.cwd(), 'views', filename)
+
+    collection[templateName] = {
+      path: templatePath
+    }
+  })
+
+  return collection
 }
